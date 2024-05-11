e7a0b76f175d9357efebda688ddae17ecee1991d
Georgi Urumov <gurumov93@gmail.com>
2024-05-11 10:37:59 +0000

    Upade sml refactoring

--- src/lib/sml.js
+++ src/lib/sml.js
@@ -39,73 +39,11 @@ export default class SML {
         }
 
         const tagTree = (currentString => {
-            const tagTree = [];
             const availableTags = this.htmlUtil.findTags(currentString);
-            const tagPairs = this.htmlUtil.pairTags(availableTags);
-            const usedIndexes = {};
-            let buildTree = (parent) => {
-                const tagTree = [];
-                if (!parent) {
-                    parent = tagPairs.find(pair => !usedIndexes[pair.id]);
-                    if (!parent) return tagTree;
-                    usedIndexes[parent.id] = true;
-                }
-                const parentTagName = parent.open.name;
-                const isComponent = !this.validHTMLElements.includes(parentTagName);
-                const attributes = {};
-                const children = [];
-                const attributeMatch = parent.string.slice(0, parent.open.endIndex - parent.open.startIndex).matchAll(this.regex.attribute);
-                for(const entry of attributeMatch){
-                    const { attribute, value } = entry.groups;
-                    attributes[attribute] = value ? value : true;
-                }
-                let textContentString = currentString.slice(parent.open.endIndex, parent.close.startIndex)
-                textContentString = textContentString.replace(this.regex.element, '');
-                if (textContentString.length && !textContentString.match(/^\s+$/g)) {
-                    children.push(textContentString);
-                }
-                
-                tagTree.push({...parent,  attributes, children, isComponent});
-                if (parent.open.type === 'selfClose') {
-                    return tagTree;
-                }
-                let directChildren = tagPairs.filter(pair => !usedIndexes[pair.id] && pair.open.startIndex >= parent.open.startIndex && pair.close.endIndex <= parent.close.endIndex).sort(sortTag);
-                if (!directChildren.length) {
-                    return tagTree;
-                }
-                while(Object.keys(usedIndexes).length < tagPairs.length) {
-                    let child = directChildren.find(pair => !usedIndexes[pair.id]);
-                    if (!child) return tagTree;
-                    usedIndexes[child.id] = true
-                    child = buildTree(child);
-                    tagTree[tagTree.length - 1].children.push(...child);
-                }
-                return tagTree;
-            }
-            while(Object.keys(usedIndexes).length < tagPairs.length) {
-                tagTree.unshift(...buildTree());
-            }
-            let surroundText = [];
-            let prevTagEndIndex = 0;
-            tagTree.forEach((parentTag, i) => {
-                let text = currentString.slice(prevTagEndIndex, parentTag.open.startIndex);
-                prevTagEndIndex = parentTag.close.endIndex
-                if (text.length && !/^\s+$/g.test(text)) {
-                    surroundText.push({text, treeIndex: i})
-                }
-                if (i === tagTree.length - 1) {
-                let postText = currentString.slice(parentTag.close.endIndex);
-                if (postText.length && !/^\s+$/g.test(postText)) {
-                    surroundText.push({text: postText, treeIndex: tagTree.length})
-                }
-                }
-            })
-            let counter = 0;
-            surroundText.forEach(entry => {
-                tagTree.splice(entry.treeIndex + counter, 0, entry.text);
-                counter++;
-            })
-            console.log(tagTree);
+            const tagPairs = this.htmlUtil.pairTags(availableTags, currentString);
+            console.log(tagPairs);
+            let tagTree = this.htmlUtil.buildTree(tagPairs, currentString);
+            tagTree = this.htmlUtil.extractTagTreeSurroundText(tagTree, currentString)
             return tagTree
         })
         let detectedElements = tagTree(linkedString);
--- src/lib/utils/HTMLInterpreters.js
+++ src/lib/utils/HTMLInterpreters.js
@@ -36,14 +36,14 @@ export default class HTMLInterpreters {
         return availableTags;
     }
 
-    pairTags(unpairedTags) {
+    pairTags(unpairedTags, htmlString) {
         const allTags = unpairedTags.map((tag, i) => ({...tag, id: i}));
         let tagPairs = [];
         const usedIndexes = {};
         // Attempt to find all tag pairs
        while(allTags.length !== usedIndexes.length) {
             let currentTag = allTags.find((tag) => !usedIndexes[tag.id]);
-            if (!currentTag) return tagPairs;
+            if (!currentTag) break;
             usedIndexes[currentTag.id] = true;
             const { name, type } = currentTag;
             if (type === 'open') {
@@ -65,7 +65,7 @@ export default class HTMLInterpreters {
             }
         }
         tagPairs = tagPairs.map((pair, i) => {
-                pair.string = currentString.slice(pair.open.startIndex, pair.close.endIndex);
+                pair.string = htmlString.slice(pair.open.startIndex, pair.close.endIndex);
                 pair.id = i;
                 return pair
         });
@@ -80,5 +80,80 @@ export default class HTMLInterpreters {
             return -1
         }
     }
+
+    buildTree(tagPairs, htmlString) {
+        const tagTree = [];
+        const usedIndexes = {};
+        let buildTree = (parent) => {
+                const tagTree = [];
+                if (!parent) {
+                    parent = tagPairs.find(pair => !usedIndexes[pair.id]);
+                    if (!parent) return tagTree;
+                    usedIndexes[parent.id] = true;
+                }
+                const parentTagName = parent.open.name;
+                const isComponent = !this.validHTMLElements.includes(parentTagName);
+                const attributes = {};
+                const children = [];
+                const attributeMatch = parent.string.slice(0, parent.open.endIndex - parent.open.startIndex).matchAll(this.regex.attribute);
+                for(const entry of attributeMatch){
+                    const { attribute, value } = entry.groups;
+                    attributes[attribute] = value ? value : true;
+                }
+                let textContentString = htmlString.slice(parent.open.endIndex, parent.close.startIndex)
+                textContentString = textContentString.replace(this.regex.element, '');
+                if (textContentString.length && !textContentString.match(/^\s+$/g)) {
+                    children.push(textContentString);
+                }
+                
+                tagTree.push({...parent,  attributes, children, isComponent});
+                if (parent.open.type === 'selfClose') {
+                    return tagTree;
+                }
+                let directChildren = tagPairs.filter(pair => !usedIndexes[pair.id] && pair.open.startIndex >= parent.open.startIndex && pair.close.endIndex <= parent.close.endIndex).sort(this.sortTagPair);
+                if (!directChildren.length) {
+                    return tagTree;
+                }
+                while(Object.keys(usedIndexes).length < tagPairs.length) {
+                    let child = directChildren.find(pair => !usedIndexes[pair.id]);
+                    if (!child) return tagTree;
+                    usedIndexes[child.id] = true
+                    child = buildTree(child);
+                    tagTree[tagTree.length - 1].children.push(...child);
+                }
+                return tagTree;
+            }
+
+            while(Object.keys(usedIndexes).length < tagPairs.length) {
+                tagTree.unshift(...buildTree());
+            }
+            return tagTree;
+
+    }
+
+    extractTagTreeSurroundText(tagTree, currentString) {
+        let surroundText = [];
+        let prevTagEndIndex = 0;
+        tagTree.forEach((parentTag, i) => {
+            let text = currentString.slice(prevTagEndIndex, parentTag.open.startIndex);
+            prevTagEndIndex = parentTag.close.endIndex
+            if (text.length && !/^\s+$/g.test(text)) {
+                surroundText.push({text, treeIndex: i})
+            }
+            if (i === tagTree.length - 1) {
+            let postText = currentString.slice(parentTag.close.endIndex);
+            if (postText.length && !/^\s+$/g.test(postText)) {
+                surroundText.push({text: postText, treeIndex: tagTree.length})
+            }
+            }
+        })
+        let counter = 0;
+        surroundText.forEach(entry => {
+            tagTree.splice(entry.treeIndex + counter, 0, entry.text);
+            counter++;
+        })
+        return tagTree;
+    }
+
 }
 
